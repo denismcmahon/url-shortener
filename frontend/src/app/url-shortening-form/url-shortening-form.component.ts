4fbeef365ad767b1a169f221a059615b221c3725
Denis McMahon <denismcmahon@gmail.com>
2024-04-16 12:45:22 +0100

    fixed validation issues

--- frontend/src/app/url-shortening-form/url-shortening-form.component.ts
+++ frontend/src/app/url-shortening-form/url-shortening-form.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { UrlHelperService } from '../services/url-helper.service';
 
 @Component({
   selector: 'app-url-shortening-form',
@@ -10,17 +11,31 @@ import { environment } from 'src/environments/environment';
 export class UrlShorteningFormComponent {
   longUrl: string;
   shortUrl: string;
+  formSubmitted: boolean = false;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private urlHelper: UrlHelperService) { }
+
+  isValidUrl(): boolean {
+    let formattedUrl = this.urlHelper.formatUrl(this.longUrl);
+    const isValidUrl = this.urlHelper.validateUrl(formattedUrl);
+    return isValidUrl;
+  }
 
   shortenUrl() {
+    this.formSubmitted = true;
     const backendUrl = `${environment.backendUrl}/api/shorten`;
-    this.http.post<{ shortUrl: string }>(backendUrl, { longUrl: this.longUrl })
+    let formattedUrl = this.urlHelper.formatUrl(this.longUrl);
+    this.http.post<{ shortUrl: string }>(backendUrl, { longUrl: formattedUrl })
       .subscribe(response => {
         this.shortUrl = response.shortUrl;
+        console.log('DM ==> response.shortUrl: ', response.shortUrl);
       }, error => {
         console.error('Error shortening URL:', error);
       });
+  }
+
+  resetFormSubmitted() {
+    this.formSubmitted = false;
   }
 
 }
